a1b1268d4c817f55100900378ded4feb020ddb40
Prakash Kumar <prakashope@gmail.com>
2023-04-16 20:49:58 +0530

    tailwind setup , and signin and home page layout initialized

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import "../../styles/global.css"
+import GlassPane from "@/components/GlassPane";
+
+export default function AuthRootLayout({children}) {
+    return <html>
+        <head />
+        <body className="h-screen w-screen rainbow-mesh p-6">
+            <GlassPane className="w-full h-full flex items-center">
+                {children}
+            </GlassPane>
+        </body>
+    </html>
+}
