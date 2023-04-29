38db6e982f8fc069bbb2e0bb9ce9fde4835f1551
Prakash Kumar <prakashope@gmail.com>
2023-04-30 00:00:10 +0530

    home page and async function

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,8 @@ const fetcher = async ({ url, method, body, json = true }) => {
     const res = await fetch(url, {
       method,
       // ...(body && JSON.stringify(body)),
-      body: JSON.stringify(body),
+      // body: JSON.stringify(body),
+      body: body && JSON.stringify(body),
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
