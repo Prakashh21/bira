435472532218cc991c12fb60fad42cf4571d25a9
Prakash Kumar <prakashope@gmail.com>
2023-05-08 23:47:44 +0530

    loader update

--- app/(dashboard)/home/loading.tsx
+++ app/(dashboard)/home/loading.tsx
@@ -1,12 +1,13 @@
 import Card from "@/components/Card";
-
+import "./styles.css"
 export default function HomePageLoader() {
 
     return (
       <div className="flex justify-center items-center w-full h-full">
-        <Card className="">
-          <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
-        </Card>
+        {/* <Card className="border-2 border-black"> */}
+          {/* <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div> */}
+          <div className="shapes-8"></div>
+        {/* </Card> */}
       </div>
     );
   }
